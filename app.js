cb8c6d886b937b1ccf2194339473adc85ffe20c5
BayMinimum <bearksb1115@gmail.com>
2017-10-22 15:57:31 +0900

    Migrate to google cloud function

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-module.exports.meal = require("./meal")
-module.exports.snack = require("./snack")
+const meal = require("./meal")
+const snack = require("./snack")
+
+module.exports.getData = function (req, res) {
+    if (req.body.type == "meal") {
+        meal((data) => {
+            res.status(200).send(JSON.stringify(data)).end()
+        })
+    } else if (req.body.type == "snack") {
+        snack((data) => {
+            res.status(200).send(data).end()
+        })
+    }
+}
