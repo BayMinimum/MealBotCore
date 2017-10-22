ecfb84b120cf15f31b624bad0cc98dbc48db5d6c
BayMinimum <bearksb1115@gmail.com>
2017-10-22 15:57:16 +0900

    Add offline snack data

--- snack.js
+++ snack.js
@@ -1,9 +1,14 @@
 // From BayMinimum/MealTweet
+const offlineData = require("./snack_offline")
 module.exports=function (callback) {
     'use strict';
     let cheerio = require('cheerio');
     let http = require('http');
     let time = require('time');
+
+    let now = new time.Date()
+    now.setTimezone("Asia/Seoul")
+    const lookupDate = generateLookupDate(now.getFullYear(), now.getMonth(), now.getDate())
 
     let options = {
         host: "gaonnuri.ksain.net",
@@ -26,26 +31,40 @@ module.exports=function (callback) {
     let parseSnack =function (html) {
         console.log(html)
         let $ = cheerio.load(html, {decodeEntities: false}); // option to avoid unicode hangul issue
-        let now = new time.Date();
-        now.setTimezone("Asia/Seoul");
+
         let dd=now.getDate();
 
         let snack = $(".snack").first().parent().find(".menu").html()
-        if(snack.indexOf("정보")>=0 && snack.indexOf("없음")>=0) snack = "";
+        if (snack.indexOf("정보") >= 0 && snack.indexOf("없음") >= 0) snack = false
         try {
             if(snack.charAt(0)===' ') snack = snack.substring(1, snack.length);
         }catch(exception){
             console.log(exception);
             console.log("Substring operation for snack failed!");
         }
-        console.log(snack);
-        callback(snack);
+        if (!snack) snack = offlineData[lookupDate]
+        if (!snack) snack = ""
+        callback(snack)
     };
 
     request.on('error', function () {
         console.log("Network error");
+        console.log(lookupDate)
+        let snack = offlineData[lookupDate]
+        if (!snack) snack = ""
+        callback(snack)
     });
 
     request.end();
 
 };
+
+
+function generateLookupDate(yyyy, mm, dd) {
+    let target = `${yyyy}-`
+    if (mm < 10) target += `0${mm}-`
+    else target += `${mm}-`
+    if (dd < 10) target += `0${dd}`
+    else target += `${dd}`
+    return target
+}
